c126cb5655b6ba52330a441aea6c35f7feafd1bd
priscilla <primcamillo@gmail.com>
2019-06-12 22:26:08 -0300

    adicionamos form no menu do restaurante

--- ComidasSchema.js
+++ ComidasSchema.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 //cada schema equivale a um collection
 const Schema = mongoose.Schema;
 const ComidasSchema = new Schema({
-    _id:{ type: mongoose.Schema.Types.ObjectId, auto:true},
-    nome: { type:String, require:true},
-        descricao: {type:String}, //opcional
-        valor: {type:Number}, //opcional
-        imagem: {type: String}
+    _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
+    nome: { type: String, require: true },
+    descricao: { type: String }, //opcional
+    valor: { type: Number }, //opcional
+    imagem: { type: String }
 })
 const comidas = mongoose.model("comidas", ComidasSchema);
 
